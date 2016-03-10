dc01e0d1d3dfc5e31daf059551b8ca766e432edf
Brian C. Mackintosh <bcmackintosh@gmail.com>
2016-03-10 16:41:10 -0500

    Refactoring, some bug fixes with "sticky" randomness, a bit cleaner allowance of choosing how variable you want randomness to be

--- catDistortion.py
+++ catDistortion.py
@@ -1,94 +1,107 @@
 import numpy as np
-# import PIL as pil
-# from scipy import misc
-import PIL
-from PIL import Image
+
 from glob import glob
+from PIL import Image
+
 import os
-from md5 import md5
+import sys
+
+from hashlib import md5
 from random import choice
 
 
+class CatContortion():
+    def __init__(self, image):
+        self.__raw = image
+        self.__transform = image
+        self.__width = self.__raw.size[0]
+        self.__height = self.__raw.size[1]
 
-def randomCrop(image):
-    # minHeight, minWidth are decimal values [0,1]
-    minWidth = 0.5
-    minHeight = 0.5
+    def img(self):
+        return self.__transform
 
-    # Make sure you leave enough room to be at least minWidth by minHeight
-    # all the +/- 1 is a quick hack to avoid the random number gen giving 0 and
-    # causing some problems
-    left = int( min(np.random.rand()*image.size[0], image.size[0]*(1-minWidth)) )+1
-    upper = int( min(np.random.rand()*image.size[1], image.size[1]*(1-minHeight)) )+1
+    def reset(self):
+        self.__set_transform(self.__raw)
 
-    right = left + int( np.random.rand()*(image.size[0]-1-left) )+1
-    lower = upper + int( np.random.rand()*(image.size[1]-1-left) )+1
+    def __set_transform(self, image):
+        self.__transform = image
+        self.__width = image.size[0]
+        self.__height = image.size[1]
 
-    return image.crop( (left, upper, right, lower) )
+    def random_crop(self, variance=.5):
+        width = self.__width
+        height = self.__height
+
+        left = int(width - (1 - np.random.rand() * variance) * width)
+        upper = int(width - (1 - np.random.rand() * variance) * width)
+
+        remaining_width = self.__width - 1 - left
+        remaining_height = self.__height - 1 - upper
+
+        right = left + int((1 - np.random.rand() * variance) * remaining_width)
+        lower = upper + int((1 - np.random.rand() * variance) * remaining_height)
+
+        self.__set_transform(self.__transform.crop( (left, upper, right, lower) ))
 
 
-def randomTranspose(image):
-    options = [
-        Image.FLIP_LEFT_RIGHT,
-        Image.FLIP_TOP_BOTTOM,
-        Image.ROTATE_90,
-        Image.ROTATE_180,
-        Image.ROTATE_270
-    ]
-    # Pick a transpose at random
-    return image.transpose( choice(options) )
+    def random_transpose(self):
+        options = [
+            Image.FLIP_LEFT_RIGHT,
+            Image.FLIP_TOP_BOTTOM,
+            Image.ROTATE_90,
+            Image.ROTATE_180,
+            Image.ROTATE_270
+        ]
+        # Pick a transpose at random
+        self.__set_transform(self.__transform.transpose(choice(options)))
 
-def randomResize(image):
-    minWidth = 0.25
-    minHeight = 0.25
+    def random_resize(self, variance=.5):
+        min_width = 1 - variance
+        min_height = 1 - variance
 
-    maxWidth = 2.0
-    maxHeight = 2.0
+        width_var = 1 / (1 - variance) - min_width
+        height_var = 1 / (1 - variance) - min_height
 
-    # Don't crop it down below minWidth by minHeight
-    return image.resize(
-        (
-            int(max(np.random.rand()*maxWidth, minWidth)*image.size[0]),
-            int(max(np.random.rand()*maxHeight, minHeight)*image.size[1])
+        # Don't crop it down below minWidth by minHeight
+        self.__set_transform(
+            self.__transform.resize((
+                int((np.random.rand() * width_var + min_width) * self.__width),
+                int((np.random.rand() * height_var + min_height) * self.__height)
+            ))
         )
-    )
 
-def blackAndWhite(image):
-    # L for black and white
-    return image.convert("L")
+    def black_and_white(self):
+        # L for black and white
+        self.__set_transform(self.__image.convert('L'))
 
-def randomNoise(image):
-    # Adjust noise level at will. Hihgher = more noise. I found even with
-    # 150 some images were very heavily distorted.
-    maxNoise = 100
-    # convert to array
-    arr = np.array(image)
-    # Add original image to noise array
-    arr = arr + np.random.poisson( np.random.rand()*maxNoise, arr.shape ).astype(np.uint8)
-    return Image.fromarray( arr )
+    def random_noise(self):
+        # Adjust noise level at will. Hihgher = more noise. I found even with
+        # 150 some images were very heavily distorted.
+        maxNoise = 100
+        # convert to array
+        arr = np.array(self.__image)
+        # Add original image to noise array
+        arr = arr + np.random.poisson(np.random.rand() * maxNoise, arr.shape).astype(np.uint8)
+        self.__set_transform(Image.fromarray(arr))
 
 if __name__ == '__main__':
-    # Get a list of images
-    files = glob("images/original/*")
-    # Will use this list as random options for transforming an image
-    functions = [ blackAndWhite, randomTranspose, randomNoise ]
-    # Extra functions are used to prefent cropping AND resizing.
-    extraFunctions = [ randomResize, randomCrop ]
-    numOfChildren = 5
-    for f in files[:]:
-        print "Opening: ",f
-        for counter in range(numOfChildren):
-            # Do at least ONE mutation
-            numOfMutations = max(int(np.random.rand()*len(functions)), 1)
-            print "Doing %d mutations"%numOfMutations
-            calls = np.random.choice( functions, numOfMutations)
-            nImg = Image.open(f)
-            for func in calls:
-                nImg = func(nImg)
-            r = np.random.rand()
-            if r < 0.4:
-                nImg = randomResize(nImg)
-            elif r < 0.8:
-                nImg = randomCrop(nImg)
+    num_of_children = 20
 
-            nImg.save( "images/distorted/%s_%s.jpg"%( f.split("/")[-1],md5(str(counter)+f).hexdigest() ) )
+    if len(sys.argv) > 1:
+        num_of_children = sys.argv[1]
+
+    # Get a list of images
+    files = glob('images/original/*')
+
+    for f in files:
+        print('Opening: ', f)
+        for counter in range(num_of_children):
+            nImg = Image.open(f)
+
+            distorted = CatContortion(nImg)
+            distorted.random_crop(variance=.5)
+            distorted.random_resize(variance=.2)
+
+            distorted.img().save('images/custom/%s_%s.jpg'%( f.split('/')[-1], md5(str(counter).encode('utf-8') + f.encode('utf-8')).hexdigest()))
+
+            distorted.reset()
